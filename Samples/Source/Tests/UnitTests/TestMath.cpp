d31f1b1aed884f3144e1e9ec2bd36f7d14086bb5
justin@raincitygames.com <justin@raincitygames.com@84234807-5fa8-7533-bdbf-dec7f933a0b8>
2011-08-04 03:21:49 +0000

    Reworked the unit test module..  It better represents the tests in a tree format, with in console logging

--- Samples/Source/Tests/UnitTests/TestMath.cpp
+++ Samples/Source/Tests/UnitTests/TestMath.cpp
@@ -7,194 +7,244 @@
 #include "BasePCH.h"
 #include "UnitTestsModule.h"
 
+
 #define COMPARE_FLOAT(a, b) (((a) >= ((b) - Math::ZERO_TOLERANCE)) && ((a) <= ((b) + Math::ZERO_TOLERANCE)))
 
 // ***********************************************************************
-void UnitTestsModule::TestMath()
+TestStatus::Enum UnitTestsModule::Test_Math_Randoms(TestExecutionContext *context)
 {
-	Clear();
-	WriteLine(LogLevel::System, L"Tests Starting");
+    #define RANDOM_SAMPLE_SIZE	100
+    TestStatus::Enum result = TestStatus::Pass;
 
-	// ===================================================================
-	// Randoms
-	{
-		WriteLine(LogLevel::Info, L"Running: Random Number Tests");
+    // Integer range
+    // -----------------------
 
-		#define RANDOM_SAMPLE_SIZE	100
+    int intArray[RANDOM_SAMPLE_SIZE];
+    int intTotal = 0;
+    int intMin = 200;
+    int intMax = 100;
+    for(int i=0; i<RANDOM_SAMPLE_SIZE; i++)
+    {
+        intArray[i] = Random::FromInterval(100, 200);
+        intTotal += intArray[i];
+        intMin = Math::Min(intMin, intArray[i]);
+        intMax = Math::Max(intMax, intArray[i]);
+    }
+    int intAvg = intTotal / RANDOM_SAMPLE_SIZE;
 
-		// Integer range
-		// -----------------------
+    context->Log->WriteLine(LogLevel::Info, L"Test: Random Int Range 100-200");
+    context->Log->WriteLine(LogLevel::Info, L" - Min: %u   Max: %u   Avg: %u", intMin, intMax, intAvg);
 
-		int intArray[RANDOM_SAMPLE_SIZE];
-		int intTotal = 0;
-		int intMin = 200;
-		int intMax = 100;
-		for(int i=0; i<RANDOM_SAMPLE_SIZE; i++)
-		{
-			intArray[i] = Random::FromInterval(100, 200);
-			intTotal += intArray[i];
-			intMin = Math::Min(intMin, intArray[i]);
-			intMax = Math::Max(intMax, intArray[i]);
-		}
-		int intAvg = intTotal / RANDOM_SAMPLE_SIZE;
+    // Verify the min,max,avg
+    if(intMin > 110) 
+    {
+        context->Log->WriteLine(LogLevel::Warning, L"Warning: Random int 100-200 minimum %u > 110", intMin); 
+        result = TestStatus::Warning;
+    }
+    if(intMax < 190) 
+    {
+        context->Log->WriteLine(LogLevel::Warning, L"Warning: Random int 100-200 maximum %u < 190", intMax); 
+        result = TestStatus::Warning;
+    }
+    if(intAvg < 140 || intAvg > 160) 
+    {
+        context->Log->WriteLine(LogLevel::Warning, L"Warning: Random int 100-200 average %u is <140 or >160", intAvg); 
+        result = TestStatus::Warning;
+    }
+    
+    // Unit [0-1] Floats
+    // --------------------------
 
-		LOG_INFO(L"Test: Random Int Range 100-200");
-		LOG_INFO(L" - Min: %u   Max: %u   Avg: %u", intMin, intMax, intAvg);
+    float unitArray[RANDOM_SAMPLE_SIZE];
+    float unitTotal = 0.0f;
+    float unitMin = 10.0f;
+    float unitMax = -10.0f;
+    for(int i=0; i<RANDOM_SAMPLE_SIZE; i++)
+    {
+        unitArray[i] = Random::GetUnit();
+        unitTotal += unitArray[i];
+        unitMin = Math::Min(unitMin, unitArray[i]);
+        unitMax = Math::Max(unitMax, unitArray[i]);
+    }
+    float unitAvg = unitTotal / RANDOM_SAMPLE_SIZE;
 
-		ASSERT(intMin < 110, L"%u < 110", intMin); 
-		ASSERT(intMax > 190, L"%u > 190", intMax); 
-		ASSERT(intAvg > 145 && intAvg < 155, L"145 < %u < 155", intAvg); 
+    context->Log->WriteLine(LogLevel::Info, L"Test: Random Float Unit [0,1]");
+    context->Log->WriteLine(LogLevel::Info, L" - Min: %f   Max: %f   Avg: %f", unitMin, unitMax, unitAvg);
 
-		// Unit [0-1] Floats
-		// --------------------------
+    // Verify the min,max,avg
+    if(unitMin > 0.1f) 
+    {
+        context->Log->WriteLine(LogLevel::Warning, L"Warning: Random unit minimum %f > 0.1", unitMin); 
+        result = TestStatus::Warning;
+    }
+    if(unitMax < 0.9f) 
+    {
+        context->Log->WriteLine(LogLevel::Warning, L"Warning: Random unit maximum %f < 0.9", unitMax); 
+        result = TestStatus::Warning;
+    }
+    if(unitAvg < 0.4f || unitAvg > 0.6f) 
+    {
+        context->Log->WriteLine(LogLevel::Warning, L"Warning: Random unit average %f is <0.4 or >0.6", unitAvg); 
+        result = TestStatus::Warning;
+    }
+    
 
-		float unitArray[RANDOM_SAMPLE_SIZE];
-		float unitTotal = 0.0f;
-		float unitMin = 10.0f;
-		float unitMax = -10.0f;
-		for(int i=0; i<RANDOM_SAMPLE_SIZE; i++)
-		{
-			unitArray[i] = Random::GetUnit();
-			unitTotal += unitArray[i];
-			unitMin = Math::Min(unitMin, unitArray[i]);
-			unitMax = Math::Max(unitMax, unitArray[i]);
-		}
-		float unitAvg = unitTotal / RANDOM_SAMPLE_SIZE;
+    // Symmetric [0-1] Floats
+    // --------------------------
 
-		LOG_INFO(L"Test: Random Float Unit [0,1]");
-		LOG_INFO(L" - Min: %f   Max: %f   Avg: %f", unitMin, unitMax, unitAvg);
+    float symArray[RANDOM_SAMPLE_SIZE];
+    float symTotal = 0.0f;
+    float symMin = 10.0f;
+    float symMax = -10.0f;
+    for(int i=0; i<RANDOM_SAMPLE_SIZE; i++)
+    {
+        symArray[i] = Random::GetSymmetric();
+        symTotal += symArray[i];
+        symMin = Math::Min(symMin, symArray[i]);
+        symMax = Math::Max(symMax, symArray[i]);
+    }
+    float symAvg = symTotal / RANDOM_SAMPLE_SIZE;
+    context->Log->WriteLine(LogLevel::Info, L"Test: Random Float Symmetric [-1,1]");
+    context->Log->WriteLine(LogLevel::Info, L" - Min: %f   Max: %f   Avg: %f", symMin, symMax, symAvg);
 
-		ASSERT(unitMin < 0.1f, L"%f < 0.1", unitMin); 
-		ASSERT(unitMax > 0.9f, L"%f > 0.9", unitMax); 
-		ASSERT(unitAvg > 0.45f && unitAvg < 0.55f, L"0.45 < %f < 0.55", unitAvg); 
+    // Verify the min,max,avg
+    if(symMin > -0.9f) 
+    {
+        context->Log->WriteLine(LogLevel::Warning, L"Warning: Random symmetric minimum %f > -0.9", symMin); 
+        result = TestStatus::Warning;
+    }
+    if(symMax < 0.9f) 
+    {
+        context->Log->WriteLine(LogLevel::Warning, L"Warning: Random symmetric maximum %f < 0.9", symMax); 
+        result = TestStatus::Warning;
+    }
+    if(symAvg < -0.1f || symAvg > 0.1f) 
+    {
+        context->Log->WriteLine(LogLevel::Warning, L"Warning: Random symmetric average %f is <-0.1 or >0.1", symAvg); 
+        result = TestStatus::Warning;
+    }
 
-		// Symmetric [0-1] Floats
-		// --------------------------
+    // Reproducable Seeds
+    // ------------------------
 
-		float symArray[RANDOM_SAMPLE_SIZE];
-		float symTotal = 0.0f;
-		float symMin = 10.0f;
-		float symMax = -10.0f;
-		for(int i=0; i<RANDOM_SAMPLE_SIZE; i++)
-		{
-			symArray[i] = Random::GetSymmetric();
-			symTotal += symArray[i];
-			symMin = Math::Min(symMin, symArray[i]);
-			symMax = Math::Max(symMax, symArray[i]);
-		}
-		float symAvg = symTotal / RANDOM_SAMPLE_SIZE;
-		LOG_INFO(L"Test: Random Float Symmetric [-1,1]");
-		LOG_INFO(L" - Min: %f   Max: %f   Avg: %f", symMin, symMax, symAvg);
+    MersenneTwister random1(123456789);
+    MersenneTwister random2(123456789);
+    
+    context->Log->WriteLine(LogLevel::Info, L"Test: Randoms from duplicate seeds");
 
-		ASSERT(symMin < -0.9f, L"%f < -0.9", symMin); 
-		ASSERT(symMax > 0.9f, L"%f > 0.9", symMax); 
-		ASSERT(symAvg > -0.1f && symAvg < 0.1f, L"-0.1 < %f < 0.1", symAvg); 
+    for(int i=0; i<RANDOM_SAMPLE_SIZE; i++)
+    {
+        float test1 = random1.GetUnit();
+        float test2 = random2.GetUnit();
+        
+        if(test1 != test2)
+        {
+            context->Log->WriteLine(LogLevel::Error, L"ERROR: Two generators with the same seed produced mismatched results"); 
+            result = TestStatus::Fail;
+            break;
+        }
+    }
 
-		// Reproducable Seeds
-		// ------------------------
+    // ---------------------------
 
-		MersenneTwister random1(123456789);
-		MersenneTwister random2(123456789);
+    MersenneTwister randomA(1);
+    MersenneTwister randomB(2);
 
-		for(int i=0; i<RANDOM_SAMPLE_SIZE; i++)
-		{
-			float test1 = random1.GetUnit();
-			float test2 = random2.GetUnit();
-			ASSERT(test1 == test2, L"Random generator reproducable seed test failed");
-		}
+    for(int i=0; i<RANDOM_SAMPLE_SIZE; i++)
+    {
+        float test1 = randomA.GetUnit();
+        float test2 = randomB.GetUnit();
+        
+        if(test1 == test2)
+        {
+            context->Log->WriteLine(LogLevel::Error, L"ERROR: Two generators with differing seeds produced the same results"); 
+            result = TestStatus::Fail;
+            break;
+        }
+    }
 
-		LOG_INFO(L"Test: Two Random instances using the same seed, produce the same results");
-
-
-		// ---------------------------
-
-		MersenneTwister randomA(1);
-		MersenneTwister randomB(2);
-
-		for(int i=0; i<RANDOM_SAMPLE_SIZE; i++)
-		{
-			float test1 = randomA.GetUnit();
-			float test2 = randomB.GetUnit();
-			ASSERT(test1 != test2, L"Random generator reproducable seed test failed");
-		}
-
-		LOG_INFO(L"Test: Two Random instances using similar seeds, produce different results");
-	}
-
-	// ===================================================================
-	// Vectors
-	{
-		WriteLine(LogLevel::Info, L"Running: Vector Tests");
-
-		LOG_INFO(L"Test: Vector2 operator & Method tests");
-
-		// CTors (and assignment
-		Vector2 a = Vector2(10,20);
-		Vector2 b(5,5);
-		Vector2 c(a);
-
-		// Comparison
-		ASSERT(!(a == b), L"Vector2 == operator failed");
-		ASSERT(a != b, L"Vector2 != operator failed");
-		
-		// Arithmetic operators
-		ASSERT((a+b) == Vector2(15,25), L"Vector2 + (Vector2) operator failed");
-		ASSERT((a-b) == Vector2(5,15), L"Vector2 - (Vector2) operator failed");
-		ASSERT((a*2) == Vector2(20,40), L"Vector2 * (scalar) operator failed");
-		ASSERT((2*a) == Vector2(20,40), L"(scalar) * Vector2 operator failed");
-		ASSERT((a/2) == Vector2(5,10), L"Vector2 / (scalar) operator failed");
-		ASSERT((-a) == Vector2(-10,-20), L"Vector2 unary negation operator failed");
-
-		// Arithmeic update operators
-		c = a; c += b;
-		ASSERT(c == Vector2(15,25), L"Vector2 += operator failed");
-		c = a; c -= b;
-		ASSERT(c == Vector2(5,15), L"Vector2 -= operator failed");
-		c = a; c *= 2;
-		ASSERT(c == Vector2(20,40), L"Vector2 *= operator failed");
-		c = a; c /= 2;
-		ASSERT(c == Vector2(5,10), L"Vector2 /= operator failed");
-
-		// Vector functions
-		float f = Vector2(3,4).Length();
-		ASSERT(COMPARE_FLOAT(f, 5), L"Vector2.Length() function failed");
-		f = Vector2(3,4).LengthSquared();
-		ASSERT(COMPARE_FLOAT(f, 25), L"Vector2.LengthSquared() function failed");
-
-		c = Vector2(-123,123);
-		c.Normalize();
-		f = c.Length();
-		ASSERT(COMPARE_FLOAT(f, 1.0f) && COMPARE_FLOAT(c.X,-c.Y), L"Vector2.Normalize() function failed");
-
-		f = Vector2(0,1).Dot(Vector2(1,0));
-		ASSERT(COMPARE_FLOAT(f, 0), L"Vector2.Dot() - failed on perpendicular vectors");
-		f = Vector2(1,1).Dot(Vector2(1,1));
-		ASSERT(COMPARE_FLOAT(f, 2), L"Vector2.Dot() - failed on parallel, equal-direction vectors");
-		f = Vector2(1,1).Dot(Vector2(-1,-1));
-		ASSERT(COMPARE_FLOAT(f, -2), L"Vector2.Dot() - failed on parallel, negative-direction vectors");
-
-		f = Vector2(1,0).GetRotationAngle();
-		ASSERT(COMPARE_FLOAT(f, 0), L"Vector2.GetDirectionAngle() - failed on 0 degree vector");
-		f = Vector2(0,1).GetRotationAngle();
-		ASSERT(COMPARE_FLOAT(f, Math::HALF_PI), L"Vector2.GetDirectionAngle() - failed on 90 degree vector");
-		f = Vector2(-1,-1).GetRotationAngle();
-		ASSERT(COMPARE_FLOAT(f, Math::PI * -0.75f), L"Vector2.GetDirectionAngle() - failed on 225 degree vector");
-		
-		c = Vector2::FromAngle(0);
-		ASSERT(COMPARE_FLOAT(c.X, 1), L"Vector2.FromAngle() - failed on 0 degree vector");
-		ASSERT(COMPARE_FLOAT(c.Y, 0), L"Vector2.FromAngle() - failed on 0 degree vector");
-		c = Vector2::FromAngle(Math::HALF_PI);
-		ASSERT(COMPARE_FLOAT(c.X, 0), L"Vector2.FromAngle() - failed on 90 degree vector");
-		ASSERT(COMPARE_FLOAT(c.Y, 1), L"Vector2.FromAngle() - failed on 90 degree vector");
-
-		// Interpolation Methods
-		// TODO
-
-		// String Methods
-		// TODO
-		
-	}
-
-	WriteLine(LogLevel::System, L"Tests Complete");
+    return result;
 }
 
+// ***********************************************************************
+TestStatus::Enum UnitTestsModule::Test_Math_Vectors(TestExecutionContext *context)
+{
+    TestStatus::Enum result = TestStatus::Pass;
+
+    // CTors (and assignment
+    Vector2 a = Vector2(10,20);
+    Vector2 b(5,5);
+    Vector2 c(a);
+
+    // Vector2 operator tests
+    // --------------------------
+    
+    context->Log->WriteLine(LogLevel::Info, L"Test: Vector2 operator tests");
+    
+    // Comparison
+    UNIT_TEST_ASSERT(!(a == b), L"Vector2 == operator failed");
+    UNIT_TEST_ASSERT(a != b, L"Vector2 != operator failed");
+    
+    // Arithmetic operators
+    UNIT_TEST_ASSERT((a+b) == Vector2(15,25), L"Vector2 + (Vector2) operator failed");
+    UNIT_TEST_ASSERT((a-b) == Vector2(5,15), L"Vector2 - (Vector2) operator failed");
+    UNIT_TEST_ASSERT((a*2) == Vector2(20,40), L"Vector2 * (scalar) operator failed");
+    UNIT_TEST_ASSERT((2*a) == Vector2(20,40), L"(scalar) * Vector2 operator failed");
+    UNIT_TEST_ASSERT((a/2) == Vector2(5,10), L"Vector2 / (scalar) operator failed");
+    UNIT_TEST_ASSERT((-a) == Vector2(-10,-20), L"Vector2 unary negation operator failed");
+
+    // Arithmeic update operators
+    c = a; c += b;
+    UNIT_TEST_ASSERT(c == Vector2(15,25), L"Vector2 += operator failed");
+    c = a; c -= b;
+    UNIT_TEST_ASSERT(c == Vector2(5,15), L"Vector2 -= operator failed");
+    c = a; c *= 2;
+    UNIT_TEST_ASSERT(c == Vector2(20,40), L"Vector2 *= operator failed");
+    c = a; c /= 2;
+    UNIT_TEST_ASSERT(c == Vector2(5,10), L"Vector2 /= operator failed");
+
+    // Vector2 Method Tests
+    // --------------------------
+    
+    context->Log->WriteLine(LogLevel::Info, L"Test: Vector2 method tests");
+    
+    // Vector functions
+    float f = Vector2(3,4).Length();
+    UNIT_TEST_ASSERT(COMPARE_FLOAT(f, 5), L"Vector2.Length() function failed");
+    f = Vector2(3,4).LengthSquared();
+    UNIT_TEST_ASSERT(COMPARE_FLOAT(f, 25), L"Vector2.LengthSquared() function failed");
+
+    c = Vector2(-123,123);
+    c.Normalize();
+    f = c.Length();
+    UNIT_TEST_ASSERT(COMPARE_FLOAT(f, 1.0f) && COMPARE_FLOAT(c.X,-c.Y), L"Vector2.Normalize() function failed");
+
+    f = Vector2(0,1).Dot(Vector2(1,0));
+    UNIT_TEST_ASSERT(COMPARE_FLOAT(f, 0), L"Vector2.Dot() - failed on perpendicular vectors");
+    f = Vector2(1,1).Dot(Vector2(1,1));
+    UNIT_TEST_ASSERT(COMPARE_FLOAT(f, 2), L"Vector2.Dot() - failed on parallel, equal-direction vectors");
+    f = Vector2(1,1).Dot(Vector2(-1,-1));
+    UNIT_TEST_ASSERT(COMPARE_FLOAT(f, -2), L"Vector2.Dot() - failed on parallel, negative-direction vectors");
+
+    f = Vector2(1,0).GetRotationAngle();
+    UNIT_TEST_ASSERT(COMPARE_FLOAT(f, 0), L"Vector2.GetDirectionAngle() - failed on 0 degree vector");
+    f = Vector2(0,1).GetRotationAngle();
+    UNIT_TEST_ASSERT(COMPARE_FLOAT(f, Math::HALF_PI), L"Vector2.GetDirectionAngle() - failed on 90 degree vector");
+    f = Vector2(-1,-1).GetRotationAngle();
+    UNIT_TEST_ASSERT(COMPARE_FLOAT(f, Math::PI * -0.75f), L"Vector2.GetDirectionAngle() - failed on 225 degree vector");
+    
+    c = Vector2::FromAngle(0);
+    UNIT_TEST_ASSERT(COMPARE_FLOAT(c.X, 1), L"Vector2.FromAngle() - failed on 0 degree vector");
+    UNIT_TEST_ASSERT(COMPARE_FLOAT(c.Y, 0), L"Vector2.FromAngle() - failed on 0 degree vector");
+    c = Vector2::FromAngle(Math::HALF_PI);
+    UNIT_TEST_ASSERT(COMPARE_FLOAT(c.X, 0), L"Vector2.FromAngle() - failed on 90 degree vector");
+    UNIT_TEST_ASSERT(COMPARE_FLOAT(c.Y, 1), L"Vector2.FromAngle() - failed on 90 degree vector");
+
+    // Interpolation Methods
+    // TODO
+
+    // String Methods
+    // TODO
+    
+    return result;
+}
--- Samples/Source/Tests/UnitTests/TestSystem.cpp
+++ Samples/Source/Tests/UnitTests/TestSystem.cpp
@@ -7,6 +7,47 @@
 // Includes
 #include "BasePCH.h"
 #include "UnitTestsModule.h"
+ 
+
+// ***********************************************************************
+TestStatus::Enum UnitTestsModule::Test_System_Logging(TestExecutionContext *context)
+{
+    context->Log->WriteLine(LogLevel::Info, L"Writing to the GDK log");
+    
+    // Enable verbose logging
+    Log::MaxLevel = LogLevel::Verbose;
+    
+    // Write some logs
+    LOG_VERBOSE(L"Verbose Log Test");
+    LOG_INFO(L"Informational Log Test");
+    LOG_WARN(L"Warning Log Test");
+    LOG_ERROR(L"Error Log Test");
+    
+    // Test log formatting
+    LOG_INFO(L"Log Format Test: str[%ls] int[%d] float[%f]", L"ABC", 123, 3.14f);
+    
+    // Test lower log levels
+    Log::MaxLevel = LogLevel::Warning;
+    LOG_VERBOSE(L"TEST FAIL - Loglevel pruning isnt working if you see this");
+    LOG_INFO(L"TEST FAIL - Loglevel pruning isnt working if you see this");
+    
+    Log::MaxLevel = LogLevel::Verbose;
+    
+    // ===================================================================
+    // Test Asserts
+    
+    int i = 0;
+    
+    // Test a valid assertion
+    ASSERT(i == 0, L"TEST FAIL - Assertions are broken if you see this");
+    
+    // Test a failing assertion
+    //ASSERT(i == 1, L"Plain assertion  (Press NO to continue testing)");
+    //ASSERT(i == 1, L"Assertion with formating: str[%ls] int[%d] float[%f]", L"ABC", 123, 3.14f);
+    
+    return TestStatus::Pass;
+}
+
 
 // ***********************************************************************
 class MemTest
@@ -16,6 +57,112 @@ public:
 	MemTest(int a) {i=a;}
 	int i;
 };
+
+// ***********************************************************************
+TestStatus::Enum UnitTestsModule::Test_System_Memory(TestExecutionContext *context)
+{
+    // Test Single object / default constructor
+    context->Log->WriteLine(LogLevel::Info, L"Single object, default constructor");
+    MemTest *memTest1 = GdkNew MemTest();
+    UNIT_TEST_ASSERT(memTest1 != NULL, L"TEST FAIL: Failed to create single instance");
+    memTest1->i = 1;
+    GdkDelete(memTest1);
+    
+    // Test: Single object, parameterized constructor
+    context->Log->WriteLine(LogLevel::Info, L"Single object, parameterized constructor");
+    MemTest *memTest2 = GdkNew MemTest(1);
+    UNIT_TEST_ASSERT(memTest2 != NULL, L"TEST FAIL: Failed to create single instance (parametric constructor)");
+    memTest2->i = 2;
+    GdkDelete(memTest2);
+    
+    // Test 1D object array
+    context->Log->WriteLine(LogLevel::Info, L"1D object array");
+    MemTest *memTest3 = GdkNew1DArray<MemTest>(10);
+    UNIT_TEST_ASSERT(memTest3 != NULL, L"TEST FAIL: Failed to 1D array");
+    memTest3[0].i = 321;
+    memTest3[1].i = 123;
+    GdkDelete1DArray(memTest3);
+    
+    // Test 2D object array
+    context->Log->WriteLine(LogLevel::Info, L"2D object array");
+    MemTest **memTest4 = GdkNew2DArray<MemTest>(2, 3);
+    UNIT_TEST_ASSERT(memTest4 != NULL, L"TEST FAIL: Failed to 2D array");
+    memTest4[0][0].i = 123;
+    memTest4[1][2].i = 321;
+    GdkDelete2DArray(memTest4);
+    
+    // Test Alloc / Free
+    context->Log->WriteLine(LogLevel::Info, L"GdkAlloc / GdkFree");
+    void* memTest5 = GdkAlloc(100);
+    UNIT_TEST_ASSERT(memTest5 != NULL, L"TEST FAIL: Failed to allocate 100 byte buffer");
+    GdkFree(memTest5);
+    
+    return TestStatus::Pass;
+}
+
+// ***********************************************************************
+TestStatus::Enum UnitTestsModule::Test_System_Containers_StringHashMap(TestExecutionContext *context)
+{
+    context->Log->WriteLine(LogLevel::Info, L"Creating StringHashMap");
+    StringHashMap<int> hashMap;
+    hashMap.Add("Foo", 3);
+    hashMap.Add("Bar", 1);
+    hashMap.Add("Gil", 4);
+    hashMap.Add("Baz", 2);
+    
+    // Find
+    context->Log->WriteLine(LogLevel::Info, L"Testing Find() method");
+    StringHashMap<int>::Iterator iter1 = hashMap.Find("Gil");
+    StringHashMap<int>::Iterator iter2 = hashMap.Find("Poo");
+    UNIT_TEST_ASSERT(iter1 != hashMap.End(), L"Failed to find an existing string hash");
+    UNIT_TEST_ASSERT(iter2 == hashMap.End(), L"Failed to not-find a missing string hash");
+    
+    // Iteration
+    context->Log->WriteLine(LogLevel::Info, L"Testing Iteration");
+    for(StringHashMap<int>::Iterator iter = hashMap.Begin(); iter != hashMap.End(); iter++)
+    {
+        UInt32 hash = iter->first;
+        int val = iter->second;
+        
+        GDK_NOT_USED(hash);
+        GDK_NOT_USED(val);
+    }
+    
+    // Removal
+    context->Log->WriteLine(LogLevel::Info, L"Testing Remove() method");
+    hashMap.Remove("Baz");
+    StringHashMap<int>::Iterator removalIter = hashMap.Find("Foo");
+    hashMap.Remove(removalIter);
+    
+}
+
+// ***********************************************************************
+TestStatus::Enum UnitTestsModule::Test_System_Containers_SortedVector(TestExecutionContext *context)
+{
+    context->Log->WriteLine(LogLevel::Info, L"Creating SortedVector");
+    
+    SortedVector<int> sv;
+    
+    sv.AddSorted(12);
+    sv.AddSorted(4);
+    sv.AddSorted(2);
+    sv.AddSorted(7);
+    sv.AddSorted(15);
+    
+    context->Log->WriteLine(LogLevel::Info, L"Testing iteration");
+    for(SortedVector<int>::iterator i = sv.begin(); i < sv.end(); i++)
+    {
+        int val = *i;
+        
+        GDK_NOT_USED(val);
+    }
+    
+    context->Log->WriteLine(LogLevel::Info, L"Testing vector is sorted");
+    UNIT_TEST_ASSERT(sv.front() == 2, L"Sorted vector is NOT sorted");
+    UNIT_TEST_ASSERT(sv.back() == 15, L"Sorted vector is NOT sorted");
+}
+
+/*
 
 // ***********************************************************************
 class ThreadTestClass
@@ -58,143 +205,6 @@ void *EventTestFunction(void * arg)
 
 	return 0;
 }
-
-
-// ***********************************************************************
-void UnitTestsModule::TestSystem()
-{
-	Clear();
-	WriteLine(LogLevel::System, L"Tests Starting");
-
-	// ===================================================================
-	// Test Logging
-	{
-		WriteLine(LogLevel::Info, L"Running: Logging Tests");
-
-		// Enable verbose logging
-		Log::MaxLevel = LogLevel::Verbose;
-
-		// Write some logs
-		LOG_VERBOSE(L"Verbose Log Test");
-		LOG_INFO(L"Informational Log Test");
-		LOG_WARN(L"Warning Log Test");
-		LOG_ERROR(L"Error Log Test");
-
-		// Test log formatting
-		LOG_INFO(L"Log Format Test: str[%ls] int[%d] float[%f]", L"ABC", 123, 3.14f);
-
-		// Test lower log levels
-		Log::MaxLevel = LogLevel::Warning;
-		LOG_VERBOSE(L"TEST FAIL - Loglevel pruning isnt working if you see this");
-		LOG_INFO(L"TEST FAIL - Loglevel pruning isnt working if you see this");
-
-		Log::MaxLevel = LogLevel::Verbose;
-
-		// ===================================================================
-		// Test Asserts
-
-		int i = 0;
-
-		// Test a valid assertion
-		ASSERT(i == 0, L"TEST FAIL - Assertions are broken if you see this");
-
-		// Test a failing assertion
-		//ASSERT(i == 1, L"Plain assertion  (Press NO to continue testing)");
-		//ASSERT(i == 1, L"Assertion with formating: str[%ls] int[%d] float[%f]", L"ABC", 123, 3.14f);
-
-	}
-	// ===================================================================
-	// Test Memory
-	{
-		WriteLine(LogLevel::Info, L"Running: Memory Tests");
-
-		// Test Single object / default constructor
-		MemTest *memTest1 = GdkNew MemTest(1);
-		ASSERT(memTest1 != NULL, L"TEST FAIL: Failed to create single instance");
-		memTest1->i = 1;
-		GdkDelete(memTest1);
-
-		// Test: Single object, parameterized contructor
-		MemTest *memTest2 = GdkNew MemTest(1);
-		ASSERT(memTest2 != NULL, L"TEST FAIL: Failed to create single instance (parametric constructor)");
-		memTest2->i = 2;
-		GdkDelete(memTest2);
-
-		// Test 1D object array
-		MemTest *memTest3 = GdkNew1DArray<MemTest>(10);
-		ASSERT(memTest3 != NULL, L"TEST FAIL: Failed to 1D array");
-		memTest3[0].i = 321;
-		memTest3[1].i = 123;
-		GdkDelete1DArray(memTest3);
-
-		// Test 2D object array
-		MemTest **memTest4 = GdkNew2DArray<MemTest>(2, 3);
-		ASSERT(memTest4 != NULL, L"TEST FAIL: Failed to 2D array");
-		memTest4[0][0].i = 123;
-		memTest4[1][2].i = 321;
-		GdkDelete2DArray(memTest4);
-
-		// Test Alloc / Free
-		void* memTest5 = GdkAlloc(100);
-		ASSERT(memTest5 != NULL, L"TEST FAIL: Failed to allocate 100 byte buffer");
-		GdkFree(memTest5);
-	}
-	// ================================================================
-	// Test StringHashmap<>
-	{
-		WriteLine(LogLevel::Info, L"Running: StringHashMap Tests");
-
-		StringHashMap<int> hashMap;
-		hashMap.Add("Foo", 3);
-		hashMap.Add("Bar", 1);
-		hashMap.Add("Gil", 4);
-		hashMap.Add("Baz", 2);
-		
-		// Find
-		StringHashMap<int>::Iterator iter1 = hashMap.Find("Gil");
-		StringHashMap<int>::Iterator iter2 = hashMap.Find("Poo");
-		ASSERT(iter1 != hashMap.End(), L"Failed to find an existing string hash");
-		ASSERT(iter2 == hashMap.End(), L"Failed to not-find a missing string hash");
-
-		// Iteration
-		for(StringHashMap<int>::Iterator iter = hashMap.Begin(); iter != hashMap.End(); iter++)
-		{
-			UInt32 hash = iter->first;
-			int val = iter->second;
-            
-            GDK_NOT_USED(hash);
-            GDK_NOT_USED(val);
-		}
-
-		// Removal
-		hashMap.Remove("Baz");
-		StringHashMap<int>::Iterator removalIter = hashMap.Find("Foo");
-		hashMap.Remove(removalIter);
-		
-	}
-	// ================================================================
-	// Test SortedVector<>
-	{
-		WriteLine(LogLevel::Info, L"Running: SortedVector Tests");
-
-		SortedVector<int> sv;
-
-		sv.AddSorted(12);
-		sv.AddSorted(4);
-		sv.AddSorted(2);
-		sv.AddSorted(7);
-		sv.AddSorted(15);
-
-		for(SortedVector<int>::iterator i = sv.begin(); i < sv.end(); i++)
-		{
-			int val = *i;
-            
-            GDK_NOT_USED(val);
-		}
-
-		ASSERT(sv.front() == 2, L"Sorted vector is NOT sorted");
-		ASSERT(sv.back() == 15, L"Sorted vector is NOT sorted");
-	}
 
 	// ================================================================
 	{
@@ -249,3 +259,8 @@ void UnitTestsModule::TestSystem()
 	WriteLine(LogLevel::System, L"Tests Complete");
 
 }
+ */
+
+
+
+
