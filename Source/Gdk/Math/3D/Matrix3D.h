94ce937d7c66123a523c2c860ac8bb507af47c78
justin@raincitygames.com <justin@raincitygames.com@84234807-5fa8-7533-bdbf-dec7f933a0b8>
2011-12-05 07:00:43 +0000

    Fixed up the PC build for the new resource management system Also fixed bug #1!!!

--- Source/Gdk/Math/3D/Matrix3D.h
+++ Source/Gdk/Math/3D/Matrix3D.h
@@ -329,7 +329,7 @@ namespace Gdk
 		float m43 = M41 * input.M13 + M42 * input.M23 + M43 * input.M33 + M44 * input.M43;
 		float m44 = M41 * input.M14 + M42 * input.M24 + M43 * input.M34 + M44 * input.M44;
 		
-		M11 = m11; M12 = m12; M13 = m13; M13 = m14;
+		M11 = m11; M12 = m12; M13 = m13; M14 = m14;
 		M21 = m21; M22 = m22; M23 = m23; M24 = m24;
 		M31 = m31; M32 = m32; M33 = m33; M34 = m34;
 		M41 = m41; M42 = m42; M43 = m43; M44 = m44;
